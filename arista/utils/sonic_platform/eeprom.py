f189a6dcd502e40b46cdbabef210cf0404f27fb5
Samuel Angebault <staphylo@arista.com>
2021-07-28 17:08:26 -0700

    sonic_platform: backport eeprom implementation
    This will prevent syseepromd from crashing as tracked by
    https://github.com/Azure/sonic-buildimage/issues/8263
    
    Change-Id: Ic269d3e8e76e463e07c8d5477b7fb6fda08f347b

--- arista/utils/sonic_platform/eeprom.py
+++ arista/utils/sonic_platform/eeprom.py
@@ -12,7 +12,35 @@ class Eeprom(EepromDecoder):
    Platform-specific Eeprom class
    """
 
-   # Work in progress
-   def __init__(self, prefdl):
-      EepromDecoder.__init__(self)
+   def __init__(self, prefdl, **kwargs):
+      EepromDecoder.__init__(self, path=None, format=None, start=0, status='',
+                             readonly=True, **kwargs)
       self._prefdl = prefdl
+
+   def read_eeprom(self):
+      return self._prefdl.data()
+
+   def prefdl(self):
+      return self._prefdl
+
+   def set_eeprom(self, e, cmd_args):
+      raise NotImplementedError
+
+   def decode_eeprom(self, e):
+      return e.show()
+
+   def is_checksum_valid(self, e):
+      return (e.isCrcValid(), e.getCrc())
+
+   def serial_number_str(self, e):
+      return e.getField('SerialNumber')
+
+   def base_mac_addr(self, e):
+      return e.getField('MAC')
+
+   def get_eeprom_dict(self, e):
+      return {'Data': e.toDict()}
+
+   def modelstr(self, e):
+      return e.getField('SKU')
+

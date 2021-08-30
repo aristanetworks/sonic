166a13b21d758a30e7ec02fe5700315a11f2f83e
Samuel Angebault <staphylo@arista.com>
2021-08-30 14:17:56 -0700

    cloverdale: force loading PSUs
    The pmbus MFR* registers on the DS460 PSUs of Cloverdale are unreliable.
    This leads to PSU initialization failing most of the time.
    The workaround here force loads the PSU on this platform if
    autodetection fails.
    
    Change-Id: Ib2249ebd4eaa0a9128d8eb47f3dda87151d9d7ce

--- arista/platforms/cloverdale.py
+++ arista/platforms/cloverdale.py
@@ -110,6 +110,7 @@ class Cloverdale(FixedSystem):
             addrFunc=addrFunc,
             presentGpio=scd.inventory.getGpio("%s_present" % name),
             led=scd.inventory.getLed(name),
+            forcePsuLoad=True,
             psus=[
                DS460,
             ],

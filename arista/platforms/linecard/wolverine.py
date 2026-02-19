ashwnsri/arista-sonic

wolverine: Add GPO 1-6 updates
Change-Id: I41e8dc6a241f8d9ec088656d39e8a1e8c24aa1d4

arista/platforms/linecard/wolverine.py
@@ -126,7 +126,7 @@ class Wolverine(DenaliLinecard):
             0x00, 0x00,
          ], description='GPI_CONFIG'),
          # GPO_CONFIG_INDEX = 11 (GPO12 DPM_JE_DISCONNECT)
-         I2cByteQuirk(0xf7, 0x0b, description='GPO_CONFIG_INDEX'),
+         I2cByteQuirk(0xf7, 0x0b, description='GPO12_CONFIG_INDEX'),
          # GPO_CONFIG to be an actively driven output with no logic
          # Pin index 0x46 maps to LGPO15
          I2cBlockQuirk(0xf8, [
@@ -135,11 +135,66 @@ class Wolverine(DenaliLinecard):
             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-         ], description='GPO_CONFIG'),
+         ], description='GPO12_CONFIG'),
          # GPIO_SELECT = 0x46 (LGPO15)
          I2cByteQuirk(0xfa, 0x46, description='GPIO_SELECT'),
          # GPIO_CONFIG the pin to be driven low
          I2cByteQuirk(0xfb, 0x03, description='GPIO_CONFIG'),
+
+         # GPO1-6 updates
+         I2cBlockQuirk(0xf7, 0x00, description='GPO1_CONFIG_INDEX'),
+         I2cBlockQuirk(0xf8, [
+            0x3e, 0x06, 0x80, 0x8f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+         ], description='GPO1_CONFIG'),
+
+         I2cByteQuirk(0xf7, 0x01, description='GPO2_CONFIG_INDEX'),
+         I2cBlockQuirk(0xf8, [
+            0x42, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x80, 0x03, 0x00, 0x00, 0xfc, 0xff, 0x7f, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+         ], description='GPO2_CONFIG'),
+
+         I2cByteQuirk(0xf7, 0x02, description='GPO3_CONFIG_INDEX'),
+         I2cBlockQuirk(0xf8, [
+            0x45, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+         ], description='GPO3_CONFIG'),
+
+         I2cByteQuirk(0xf7, 0x03, description='GPO4_CONFIG_INDEX'),
+         I2cBlockQuirk(0xf8, [
+            0x3f, 0x06, 0x00, 0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+         ], description='GPO4_CONFIG'),
+
+         I2cByteQuirk(0xf7, 0x04, description='GPO5_CONFIG_INDEX'),
+         I2cBlockQuirk(0xf8, [
+            0x43, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x03, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+         ], description='GPO5_CONFIG'),
+
+         I2cByteQuirk(0xf7, 0x05, description='GPO6_CONFIG_INDEX'),
+         I2cBlockQuirk(0xf8, [
+            0x44, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1c, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+         ], description='GPO6_CONFIG'),
       ] if self.getHwApi() > HwApi(41) else []
 
       self.control.newComponent(Ucd90320, addr=self.pca.i2cAddr(0x11), causes=[

aristanetworks/sonic

arista/platforms/linecard/wolverine.py
@@ -126,7 +126,7 @@ class Wolverine(DenaliLinecard):
             0x00, 0x00,
          ], description='GPI_CONFIG'),
          # GPO_CONFIG_INDEX = 11 (GPO12 DPM_JE_DISCONNECT)
-         I2cByteQuirk(0xf7, 0x0b, description='GPO_CONFIG_INDEX'),
+         I2cByteQuirk(0xf7, 0x0b, description='GPO12_CONFIG_INDEX'),
          # GPO_CONFIG to be an actively driven output with no logic
          # Pin index 0x46 maps to LGPO15
          I2cBlockQuirk(0xf8, [
@@ -135,11 +135,66 @@ class Wolverine(DenaliLinecard):
             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-         ], description='GPO_CONFIG'),
+         ], description='GPO12_CONFIG'),
          # GPIO_SELECT = 0x46 (LGPO15)
          I2cByteQuirk(0xfa, 0x46, description='GPIO_SELECT'),
          # GPIO_CONFIG the pin to be driven low
          I2cByteQuirk(0xfb, 0x03, description='GPIO_CONFIG'),
+
+         # GPO1-6 updates
+         I2cByteQuirk(0xf7, 0x00, description='GPO1_CONFIG_INDEX'),
+         I2cBlockQuirk(0xf8, [
+            0x3e, 0x06, 0x80, 0x8f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+         ], description='GPO1_CONFIG'),
+
+         I2cByteQuirk(0xf7, 0x01, description='GPO2_CONFIG_INDEX'),
+         I2cBlockQuirk(0xf8, [
+            0x42, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x80, 0x03, 0x00, 0x00, 0xfc, 0xff, 0x7f, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+         ], description='GPO2_CONFIG'),
+
+         I2cByteQuirk(0xf7, 0x02, description='GPO3_CONFIG_INDEX'),
+         I2cBlockQuirk(0xf8, [
+            0x45, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+         ], description='GPO3_CONFIG'),
+
+         I2cByteQuirk(0xf7, 0x03, description='GPO4_CONFIG_INDEX'),
+         I2cBlockQuirk(0xf8, [
+            0x3f, 0x06, 0x00, 0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+         ], description='GPO4_CONFIG'),
+
+         I2cByteQuirk(0xf7, 0x04, description='GPO5_CONFIG_INDEX'),
+         I2cBlockQuirk(0xf8, [
+            0x43, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x03, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+         ], description='GPO5_CONFIG'),
+
+         I2cByteQuirk(0xf7, 0x05, description='GPO6_CONFIG_INDEX'),
+         I2cBlockQuirk(0xf8, [
+            0x44, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1c, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+         ], description='GPO6_CONFIG'),
       ] if self.getHwApi() > HwApi(41) else []
 
       self.control.newComponent(Ucd90320, addr=self.pca.i2cAddr(0x11), causes=[
10f801f5054821faf6a45f11a0f3e4e7590da444
arista-hpandya <hpandya@arista.com>
2026-08-17 21:46:17 +0000

    raven: remove raven fan driver and cpu component
    Fixes: BUG1942035
    Change-Id: I9a595ccf05172ee91280c1305ebabc6dd311f75c

--- arista/components/cpu/raven.py
+++
@@ -1,8 +0,0 @@
-from ...core.component import Priority
-from ...core.component.component import Component
-
-from ...drivers.raven import RavenFanKernelDriver
-
-class RavenFanComplex(Component):
-   PRIORITY = Priority.THERMAL
-   DRIVER = RavenFanKernelDriver
--- arista/drivers/raven.py
+++
@@ -1,6 +0,0 @@
-
-from ..core.driver.kernel import KernelDriver
-
-class RavenFanKernelDriver(KernelDriver):
-   MODULE = 'raven-fan-driver'
-   PATH = '/sys/devices/platform/sb800-fans'

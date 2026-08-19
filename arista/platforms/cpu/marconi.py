986748a38beb1c2f27897adc2fe6b2244be49be3
Justin Oliver <justinoliver@arista.com>
2026-08-19 20:56:24 +0000

    marconi: updates to platform definition
    Add DPM, VRM, and TMP75 sensors to platform definition. Update PCI
    ports.
    
    Change-Id: Id8272401dba29fa5d224eccd963877de3ab19b1b
    (cherry picked from commit 19154de94a804f3bb0304086792cee3a04b4c193)

--- arista/platforms/cpu/marconi.py
+++ arista/platforms/cpu/marconi.py
@@ -1,24 +1,30 @@
 from ...core.cpu import Cpu
 from ...core.pci import PciPortDesc, PciRoot
 
-from ...components.scd import Scd, ScdSmbusDesc
 from ...components.cpld import SysCpld
+from ...components.dpm.ucd import Ucd90320, UcdGpi, UcdPriority
+from ...components.lm75 import Tmp75
+from ...components.scd import Scd, ScdSmbusDesc
+from ...components.vrm.tda38740 import Xdpe1e496b
+
+from ...descs.cause import ReloadCauseDesc
+from ...descs.sensor import Position, SensorDesc
 
 class MarconiCpu(Cpu):
 
    PLATFORM = 'marconi'
    SID = ['Marconi']
 
-   PCI_PORT_ASIC0 = PciPortDesc(0x2, 0, bus=0x14)
-   PCI_PORT_ASIC1 = PciPortDesc(0x3, 0, bus=0x14)
-   PCI_PORT_SCD0 = PciPortDesc(0x13, 0)
-   PCI_PORT_SCD1 = PciPortDesc(0x10, 0)
+   PCI_PORT_ASIC0 = PciPortDesc(0x1, 3)
+   PCI_PORT_ASIC1 = PciPortDesc(0x1, 5)
+   PCI_PORT_SCD0 = PciPortDesc(0x3, 2)
+   PCI_PORT_SCD1 = PciPortDesc(0x3, 5)
 
+   SMBUS_BMC = ScdSmbusDesc(0, 5)
    SMBUS_SC = ScdSmbusDesc(1, 0)
    SMBUS_POL = ScdSmbusDesc(1, 1)
    SMBUS_PWR = ScdSmbusDesc(1, 2)
    SMBUS_FC = ScdSmbusDesc(1, 3)
-   SMBUS_BMC = ScdSmbusDesc(0, 5)
 
    def __init__(self, **kwargs):
       super().__init__(**kwargs)
@@ -27,11 +33,10 @@ class MarconiCpu(Cpu):
 
       # TODO: add CPU and/or PCH temperature, likely from components.cpu.intel
 
-      port = self.pciRoot.pciBridge(device=0x10, func=0).downstreamPort(0)
+      port = self.pciRoot.pciBridge(device=0x2, func=2).downstreamPort(0)
       self.cpld = cpld = port.newComponent(Scd, addr=port.addr)
       cpld.createPowerCycle()
-      cpld.addSmbusMasterRange(0x8000, 1, 0x80, 6)
-      cpld.addSmbusMasterRange(0x8080, 2, 0x80, 6)
+      cpld.addSmbusMasterRange(0x8000, 2, 0x80, 7)
 
       bus = self.getSmbus(self.SMBUS_SC)
       self.syscpld = cpld.newComponent(SysCpld, addr=bus.i2cAddr(0x23))
@@ -45,11 +50,53 @@ class MarconiCpu(Cpu):
       # TODO: leak detection if it's managed by the CPU should have some bits
       # and pieces here
 
-      # TODO: add smbus front panel temp sensor cpld/0/0/0x48
+      # VRM
+      vrmTempParams = {'target': 85, 'overheat': 100, 'critical': 125}
+      vrmRails = [
+         'POS1V1_MEM',
+         'POS0V8_VDDCR',
+         'POS0V8_VDDMISC',
+         'POS0V8_VDDSOC',
+      ]
+      self.cpld.newComponent(
+         Xdpe1e496b,
+         addr=self.cpld.i2cAddr(2, 0x68),
+         sensors=[
+            SensorDesc(
+             diode=diodeId,
+             name='CPU VRM %s' % name,
+             position=Position.OTHER,
+             **vrmTempParams) for diodeId, name in enumerate(vrmRails)
+      ])
 
-   def addCpuDpm(self):
-      # TODO: add Ucd90320, cpld/0/1/0x31
-      pass
+      # TODO: Add power rails and temp sensor
+      self.cpld.newComponent(
+         Ucd90320,
+         addr=self.cpld.i2cAddr(1, 0x31),
+         causes=[
+            # TODO: update reload cause list
+            UcdGpi(6, ReloadCauseDesc.OVERTEMP),
+            UcdGpi(7, ReloadCauseDesc.CPU, priority=UcdPriority.LOW),
+      ])
+
+      # Front panel shim board
+      self.cpld.newComponent(
+         Tmp75,
+         addr=self.cpld.i2cAddr(0, 0x48),
+         sensors=[
+            SensorDesc(diode=0, name='Front panel', position=Position.INLET,
+                       target=55, overheat=65, critical=70),
+         ]
+      )
+      # CPU board ambient
+      self.cpld.newComponent(
+         Tmp75,
+         addr=self.cpld.i2cAddr(6, 0x49),
+         sensors=[
+            SensorDesc(diode=0, name='Ambient', position=Position.INLET,
+                       target=55, overheat=65, critical=70),
+         ]
+      )
 
    def getSmbus(self, desc):
       busPerMaster = next(iter(self.cpld.smbusMasters.values()))['bus']

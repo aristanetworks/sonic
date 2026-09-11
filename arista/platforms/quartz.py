77ae8c6b2499065f4b1c56dab9c871eb41a39d58
arista-hpandya <hpandya@arista.com>
2026-09-11 22:21:08 +0000

    quartz: add seu reboot cause
    PartialFixes: BUG1978154
    Change-Id: I038e513de619be5616e94f98a759d4b549719f0c

--- arista/platforms/quartz.py
+++ arista/platforms/quartz.py
@@ -48,6 +48,7 @@ class QuartzDd(FixedSystem):
             UcdGpi(4, ReloadCauseDesc.REBOOT),
             UcdGpi(5, ReloadCauseDesc.WATCHDOG),
             UcdGpi(6, ReloadCauseDesc.OVERTEMP),
+            UcdGpi(7, ReloadCauseDesc.SEU, 'SCD CRC error'),
             UcdGpi(8, ReloadCauseDesc.CPU,
                    altSource=ReloadCauseAltSource.CPU),
       ], causePriority=UcdPriority.HARDWARE_MAIN)
